d44b2e60a551226e610a9271158aecb25bb5aa21
Shristi Bhandari <sa23919@student.samk.fi>
2024-05-08 12:16:06 +0300

    python_programming

---
+++ .ipynb_checkpoints/main_program-checkpoint.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+
+#from function_no_name_is_main import useless_function
+from function_file import useless_function
+
+if __name__ == "__main__":
+
+    print(useless_function(10,10))
+    print(useless_function(30,4))
